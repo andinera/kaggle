aa197e7eb8ac0dfcf0f88b46145105c3e8b61e3d
Andinera Ananke <andinera.ananke@gmail.com>
2025-04-21 17:42:18 -0600

    Move files into titanic

--- titanic/notebook.py
+++ titanic/notebook.py
@@ -1,14 +1,67 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
 import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
+
+
+
+
+
 
 train_data = pd.read_csv("/kaggle/input/titanic/train.csv")
+train_data.head()
+
+
+
+
+
+
+
+
 test_data = pd.read_csv("/kaggle/input/titanic/test.csv")
+test_data.head()
 
 
 
 
-# Tutorial - Random Forest
+
+
+
+
+women = train_data.loc[train_data.Sex == 'female']["Survived"]
+rate_women = sum(women)/len(women)
+
+print("% of women who survived:", rate_women)
+
+
+
+
+
+
+men = train_data.loc[train_data.Sex == 'male']["Survived"]
+rate_men = sum(men)/len(men)
+
+print("% of men who survived:", rate_men)
+
+
+
+
+
+
+
 
 from sklearn.ensemble import RandomForestClassifier
 
@@ -25,11 +78,15 @@ predictions = model.predict(X_test)
 output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
 print(output)
 output.to_csv('submission.csv', index=False)
+print("Your submission was successfully saved!")
 
 
 
 
-# Linear Regression
+
+
+
+
 
 from sklearn.linear_model import LinearRegression
 
@@ -47,43 +104,44 @@ y_pred_rounded = np.round(y_pred)
 
 output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': y_pred_rounded}).astype(int)
 output.to_csv('submission.csv', index=False)
+print("Your submission was successfully saved!")
 
 
 
 
-# Linear Regression
-# Split up train data into train/test to evaluate different models before submitting
-
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
-
-y_train = train_data_80["Survived"]
-y_test = test_data_20["Survived"]
-
-features = ["Pclass", "Sex"]
-X_train = pd.get_dummies(train_data_80[features])
-X_test = pd.get_dummies(test_data_20[features])
-
-model = LinearRegression()
-model.fit(X_train, y_train)
-
-y_pred = model.predict(X_test)
-y_pred_rounded = np.round(y_pred)
-
-# Evaluate the model
-mse = mean_squared_error(y_test, y_pred)
-rmse = np.sqrt(mse)
-mae = mean_absolute_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
-
-# Print the evaluation metrics
-print("Mean Squared Error (MSE):", mse)
-print("Root Mean Squared Error (RMSE):", rmse)
-print("Mean Absolute Error (MAE):", mae)
-print("R-squared (R²):", r2)
 
 
 
 
-# Logistic Regression
 
+
+from sklearn.tree import DecisionTreeClassifier
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import accuracy_score
+# import pandas as pd
+
+# # Sample data (replace with your actual data)
+# data = {'feature1': [1, 2, 3, 4, 5], 
+#         'feature2': [5, 4, 3, 2, 1],
+#         'target':   [0, 1, 0, 1, 0]}
+# df = pd.DataFrame(data)
+
+# # Split data into features (X) and target (y)
+# X = df[['feature1', 'feature2']]
+# y = df['target']
+
+# # Split data into training and testing sets
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# # Create a decision tree classifier
+# dt_classifier = DecisionTreeClassifier()
+
+# # Train the classifier
+# dt_classifier.fit(X_train, y_train)
+
+# # Make predictions on the test set
+# y_pred = dt_classifier.predict(X_test)
+
+# # Evaluate the model
+# accuracy = accuracy_score(y_test, y_pred)
+# print("Accuracy:", accuracy)
